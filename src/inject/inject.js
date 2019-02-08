d260716d2d3f8ab61f04d2df9850860f88316e2e
aggarwalankush <ankushagg93@gmail.com>
2019-02-07 23:47:50 -0800

    fixed nav

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,5 +1,5 @@
 var profileName = document.querySelector('.header-nav-current-user strong').textContent;
-var navigationList = document.querySelector('ul[role="navigation"]');
+var navigationList = document.querySelector('nav > ul');
 var className = navigationList.querySelector('li > a').className;
 var profileButton = '<li><a href="/' + profileName + '" class="' + className + '">Profile</a></li>';
 navigationList.innerHTML += profileButton;
